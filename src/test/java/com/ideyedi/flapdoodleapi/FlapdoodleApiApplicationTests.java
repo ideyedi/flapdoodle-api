7c28f256cad0099d774c0a981ec3a44f9a8090cc
enrique <gdregas18@gmail.com>
2024-03-19 15:46:47 +0900

    flapdoodle 동작 확인

--- src/test/java/com/ideyedi/flapdoodleapi/FlapdoodleApiApplicationTests.java
+++ src/test/java/com/ideyedi/flapdoodleapi/FlapdoodleApiApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.util.Optional;
+
 @SpringBootTest
 class FlapdoodleApiApplicationTests {
 
